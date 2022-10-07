b9bf336f52e698198aae21304797b56a86f25f41
yamato0211 <kumayama0211@gmail.com>
2022-10-08 07:37:53 +0900

    [add] firstcommit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_cors::Cors;
+
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_header()
+            .allow_any_origin()
+            .allow_any_method();
+        App::new()
+            .wrap(cors)
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
+}
