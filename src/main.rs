57d80a69b51907251ffa4704d83fb837a5e49c5b
yamato0211 <kumayama0211@gmail.com>
2022-10-10 01:21:51 +0900

    [add] lib.rsとの連携

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,71 @@
-extern crate env_logger;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder,middleware::Logger};
 use actix_cors::Cors;
-
+use actix_web::{
+    App,HttpServer,Responder,HttpResponse,get,
+    middleware::{
+        Compress,
+        Logger,
+    },
+    web::{
+        self,
+        Data,
+    },
+};
+use anyhow::Result;
+use dotenv::dotenv;
+use graphql::{
+    graphiql,
+    graphql,
+    playground,
+    schemas::create_schema,
+};
+use std::{
+    env,
+    sync::Arc,
+};
 
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn health() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[actix_rt::main]
+async fn main() -> Result<()> {
+    // .envに記述された環境変数の読み込み.
+    dotenv().ok();
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
+    // debugと同等以上の重要度を持つログを表示するように設定し、ログを開始する.
+    env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    HttpServer::new(|| {
-        let cors = Cors::default()
-            .allow_any_header()
-            .allow_any_origin()
-            .allow_any_method();
+    // Schemaオブジェクトをスレッドセーフな型でホランラップする.
+    let schema = Arc::new(create_schema());
+
+    // サーバーの色んな設定.
+    let mut server = HttpServer::new(move || {
         App::new()
-            .wrap(cors)
+            .app_data(Data::from(schema.clone()))
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allow_any_header()
+                    .allow_any_method()
+                    .supports_credentials()
+            )
+            .wrap(Compress::default())
             .wrap(Logger::default())
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("0.0.0.0", 8000))?
-    .run()
-    .await
+            .service(health)
+            .service(
+                web::resource("/graphql")
+                    .route(web::get().to(graphql))
+                    .route(web::post().to(graphql))
+            )
+            // /graphiqlエンドポイントにgraphiql()をセットする.
+            .service(web::resource("/graphiql").route(web::get().to(graphiql)))
+            // /playgroundエンドポイントにplayground()をセットする.
+            .service(web::resource("/playground").route(web::get().to(playground)))
+    });
+    server = server.bind(("0.0.0.0", 8000))?;
+    server.run().await?;
+
+    Ok(())
 }
