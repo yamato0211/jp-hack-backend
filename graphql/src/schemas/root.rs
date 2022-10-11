6482d03a525aa9c51e328bb7ef2afd7d8bfe0ebd
yamato0211 <kumayama0211@gmail.com>
2022-10-12 00:11:52 +0900

    [add] app_dataからコネクションプールを拾う

--- graphql/src/schemas/root.rs
+++ graphql/src/schemas/root.rs
@@ -1,3 +1,7 @@
+use crate::db::{
+    PgPool,
+};
+ use actix_web::web::Data;
 use juniper::{EmptySubscription, RootNode};
 
 // 後々ジェネリクスの引数とかに使うので、型をまとめておく.
@@ -5,6 +9,7 @@ pub type Schema = RootNode<'static, Query, Mutation, EmptySubscription<Context>>
 
 pub struct Context {
     pub token: Option<String>,
+    pub pool: Data<PgPool>,
 }
 
 // 「GraphQLのコンテキスト」という特徴を付与する.
