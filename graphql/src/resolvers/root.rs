cd9acab2cb994552c41557e18f93f9a9b7f6c6fd
yamato0211 <kumayama0211@gmail.com>
2022-10-12 00:14:02 +0900

    [add] add new function to Query and Mutation

--- graphql/src/resolvers/root.rs
+++ graphql/src/resolvers/root.rs
@@ -1,27 +1,54 @@
 use crate::{
+    db::users,
     schemas::{
         root::{
             Context,
             Mutation,
             Query,
         },
+        user::{
+            User,
+            NewUser,
+        },
     },
 };
 use juniper::{
+    FieldResult,
     graphql_object,
 };
+
+use uuid::Uuid;
 
 // 「GraphQLのオブジェクト型」という特徴を付与する.
 #[graphql_object(context=Context)]
 impl Query {
-    fn dummy_query() -> String {
-        String::from("It is dummy query.")
+    fn get_user(context: &Context, id: Uuid) -> FieldResult<User> {
+        let user = users::Cruds::find_by_id(&context.pool, id)?;
+        Ok(user.into())
+    }
+
+    fn hello() -> String {
+        String::from("World!!")
+    }
+
+    async fn list_user(context: &Context) -> FieldResult<Vec<User>> {
+        let users = users::Cruds::all_user(&context.pool)?;
+
+        Ok(users.into_iter().map(|u| u.into()).collect())
     }
 }
 
 #[graphql_object(context=Context)]
 impl Mutation {
-    fn dummy_mutation() -> String {
-        String::from("It is dummy mutation.")
+    fn create_user(context: &Context, new_user: NewUser) -> FieldResult<User> {
+        let user = users::Cruds::insert_user(&context.pool, new_user.into())?;
+ 
+        Ok(user.into())
+    }
+
+    fn delete_user(context: &Context, id: Uuid) -> FieldResult<User> {
+        let user = users::Cruds::delete_user(&context.pool, id)?;
+ 
+        Ok(user.into())
     }
 }
